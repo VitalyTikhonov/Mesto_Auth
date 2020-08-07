dfc3bbc06006c027589d7d18f13ba230f62421b4
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2020-08-07 21:13:49 +0400

    S13 C05 Made the users and users/:id get processing

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,4 @@
 const User = require('../models/user');
-const { readFileAsset, searchForUser, sendWholeJson } = require('../helpers/helpers.js');
 
 function createUser(req, res) {
   const { name, about, avatar } = req.body;
@@ -10,27 +9,15 @@ function createUser(req, res) {
 }
 
 function getAllUsers(req, res) {
-  sendWholeJson('users.json', res);
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 }
 
 function getSingleUser(req, res) {
-  const usersReadStream = readFileAsset('users.json', res);
-  let users = '';
-
-  usersReadStream.on('data', (data) => {
-    users += data;
-  });
-
-  usersReadStream.on('end', () => {
-    users = JSON.parse(users);
-
-    if (!searchForUser(users, [req.params.id])) {
-      res.status(404).send({ message: 'Нет пользователя с таким id' });
-      return;
-    }
-
-    res.send(searchForUser(users, [req.params.id]));
-  });
+  User.findById(req.params.id)
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 }
 
 module.exports = {
--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,29 +1,9 @@
-const path = require('path');
-const fs = require('fs');
+// const path = require('path');
+// const fs = require('fs');
 const bodyParser = require('body-parser');
 
 const bodyParserJson = bodyParser.json({ type: 'application/json' });
 
-function readFileAsset(fileName, res) {
-  const readStream = fs.createReadStream(path.join(__dirname, '../data', fileName), { encoding: 'utf8' });
-  readStream.on('error', () => {
-    res.status(500).json({ error: 'На сервере произошла ошибка' });
-  });
-  return readStream;
-}
-
-function searchForUser(array, id) {
-  return array.find((user) => user._id === id.toString());
-}
-
-function sendWholeJson(file, res) {
-  res.set({ 'content-type': 'application/json; charset=utf-8' });
-  readFileAsset(file, res).pipe(res);
-}
-
 module.exports = {
   bodyParserJson,
-  readFileAsset,
-  searchForUser,
-  sendWholeJson,
 };
