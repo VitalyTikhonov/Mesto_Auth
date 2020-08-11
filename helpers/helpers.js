e89fed959e0111863199dd7d258e3646951fb718
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2020-08-11 19:36:28 +0400

    S13 C20 Revived the idea of the CONFIG: fixed the argument names issue, debugged the first paths.

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,33 +1,3 @@
-/*
-Предпринята попытка реализовать сложный объект-карту (configMap), который бы служил
-единым интерфейсом, позволяющим передавать любую комбинацию параметров в контроллеры,
-точнее в единую функцию (controllerPromiseHandler), обрабатывающую промисы контроллеров.
-К сожалению, на данный момент не работает.
-В Run.js работало при таком же синтаксисе передачи аругментов через деструктуризацию,
-но без методов send в объекте configMap. Ошибка возникает не на этапе загрузки файла,
-а в том случае, если происходит обращение к методам configMap из нижестоящей функции.
-Из чего я делаю вывод, что проблема не в неопределяемости метода send в configMap,
-а в потере контекста?
-
-Текущая реализация предполагает, что на основе объекта-карты вручную создается объект-конфиг
-для конкретного контроллера. Он наряду с функцией – обработчиком промиса
-экспортируется, передается в методы контроллера, а оттуда – (как это ни странно)
-своему "корешу" – обработчику промиса.
-Пришлось попробовать такой вариант вынужденно:
-изначально предполагалось создавать конфиг непосредственно в контроллере. Но так
-указываемые там отсылки к методам configMap не определялись (в файле контроллера возникала
-ошибка configMap is not defined и функция, вызванная по ним из файла helpers,
-не определялась => превратил их (отсылки, значения свойств конфига) в строки и пытался
-превращать их уже в helpers в функции – ломалась сложная система передачи параметров =>
-поэтому попытался перенести создание конфига в helpers).
-Возможен и другой вариант. Объект-конфиг создавать здесь же,
-передавать его в создаваемый здесь же вариант функции – обработчика промисов
-controllerPromiseHandler. Таким образом, предусматривается по варианту объекта-конфига
-(как и ранее) и варианту controllerPromiseHandler для каждого контроллера.
-
-В данный момент целесообразность дальнейшей разработки в этом направлении сомнительна.
-*/
-
 const errors = {
   byField: {
     name: 'Ошибка в поле Name.',
@@ -63,6 +33,10 @@ const configMap = {
   },
   send: {
     DBObject: ({ res, respObj }) => { res.send({ data: respObj }); },
+    // DBObject: ({ res, respObj }) => {
+    // res.send({ data: respObj });
+    // console.log('res', res);
+    // },
     error: {
       noDoc: ({ res, doc }) => {
         res.status(404).send({ message: `${errors.byDocType[doc]}` });
@@ -71,7 +45,6 @@ const configMap = {
         res.status(500).send({ message: `На сервере произошла ошибка: ${err.message}` });
       },
       invalidData: ({ res, err }) => {
-        // console.log('res', res);
         // console.log('err', err);
         // console.log('typeof err', typeof err);
         res.status(400).send({ message: joinErrorMessages(errors.byField, err) });
@@ -79,6 +52,8 @@ const configMap = {
     },
   },
 };
+
+// function configMapSendDBObject() { }
 
 /*
 const config = {
@@ -123,9 +98,9 @@ const createUserConfig = {
 
 function controllerPromiseHandler(promise, req, res, options) {
   promise
-    .then((responseObject) => {
-      if (options.then.check(responseObject)) {
-        options.then.ifTrue(options.arguments);
+    .then((respObj) => {
+      if (options.then.check(respObj)) {
+        options.then.ifTrue({ res, respObj, ...options.arguments });
       } else {
         options.then.ifFalse(options.arguments);
       }
