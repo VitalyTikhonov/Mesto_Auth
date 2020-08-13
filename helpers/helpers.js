330fbeeb2c344df81267324d91ccd233cba5019e
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2020-08-13 15:47:26 +0400

    S13 C22 Prepared to do without the Config To keep the Config for reference only.

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -28,48 +28,37 @@ function joinErrorMessages(fieldErrorMap, actualError) {
 const configMap = {
   check: {
     no: (checkable) => checkable, // нужно просто true
-    respObj: (respObj) => respObj === null, // check.responseObj(respObj),
+    // configMap.check.no(checkable)
+    orFail: ({ res, doc }) => {
+      res.status(404).send({ message: `${errors.byDocType[doc]}` });
+    },
+    // .orFail(configMap.check.orFail(res, doc))
     errObj: (errorType) => errorType === 'ObjectId', // check.errorObj(err.kind)
+    // configMap.check.errObj(errorType)
   },
   send: {
     DBObject: ({ res, respObj }) => { res.send({ data: respObj }); },
+    // configMap.send.DBObject(res, respObj)
     error: {
       noDoc: ({ res, doc }) => {
         res.status(404).send({ message: `${errors.byDocType[doc]}` });
       },
+      // configMap.send.error.noDoc(res, doc)
       server: ({ res, err }) => {
         res.status(500).send({ message: `На сервере произошла ошибка: ${err.message}` });
       },
+      // configMap.send.error.server(res, err)
       invalidData: ({ res, err }) => {
         res.status(400).send({ message: joinErrorMessages(errors.byField, err) });
       },
+      // configMap.send.error.invalidData(res, err)
     },
   },
 };
-/*
-configMap.check.no(checkable)
-configMap.check.respObj(respObj)
-configMap.check.errObj(errorType)
-configMap.send.DBObject(res, respObj)
-configMap.send.error.noDoc(res, doc)
-configMap.send.error.server(res, err)
-configMap.send.error.invalidData(res, err)
- */
 function createUserHandler(promise, req, res) {
   promise
-    .then((respObj) => {
-      if ( (respObj)) {
-        then.ifTrue({ res, respObj, ...arguments });
-      } else {
-        then.ifFalse(arguments);
-      }
-    })
+    .then()
     .catch((err) => {
-      if (catch.check(err)) {
-        catch.ifTrue({ res, err, ...arguments });
-      } else {
-        catch.ifFalse({ res, err, ...arguments });
-      }
     });
 }
 
